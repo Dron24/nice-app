8c638375df467fac2e4b61b09ac23ddcc4e88662
Dron24 <stepanoff888@yandex.ru>
2023-03-31 23:35:52 +0500

    Тест_003

--- src/App/Modules/Footer/Footer.jsx
+++ src/App/Modules/Footer/Footer.jsx
@@ -1,9 +1,20 @@
 import st from './Footer.module.css';
+import fakeImg from './fake.jpg'
 
 function Footer() {
   return (
     <div className={st.footer}>
-        Footer
+        <div className={st.footerBox}>
+          <div className={st.item}>
+            &copy; 2023 Степанов А. А. <span>All rights reserved</span>
+          </div>
+          <div className={st.item}>
+            Пустой бокс
+          </div>
+          <div className={st.item}>
+            <img src={fakeImg} alt="fakeImg"/>
+          </div>
+        </div>
     </div>
   );
 }
